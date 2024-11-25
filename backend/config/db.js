29851240ac63ea24d4e48d8ab55f0ac9f3c67abe
vinayakbhovi360 <vinayakbhovi360@gmail.com>
2024-11-25 12:02:46 +0530

    check

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,8 @@ import mongoose, { connect } from "mongoose";
 // mongodb+srv://vinayakbhovi56:Vinayak1234@cluster0.qkabk.mongodb.net/?
 // retryWrites=true&w=majority&appName=Cluster0
 
+// vinayakbhovi360 iyP08eiZRoiFyhQu  
+
 export const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI).then(() => console.log("DB connect"))
 }
